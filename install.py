fe6636b201a87566418cef8027f6198a3af962f4
xushuai_rmbp <xushvai@gmail.com>
2015-11-08 02:17:35 +0800

    install script.
    Signed-off-by: xushuai_rmbp <xushvai@gmail.com>

---
+++ install.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import git
+import os
+# git.Git().clone("https://github.com/Rykka/clickable.vim")
+
+os.system("git clone https://github.com/Rykka/clickable.vim")
+

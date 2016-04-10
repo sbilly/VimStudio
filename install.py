5e344d4307a13076f65a29143758a46a85c5c858
xushvai <xushuai@xus.io>
2016-04-10 23:26:46 +0800

    add fcitx plugin.
    Signed-off-by: xushvai <xushuai@xus.io>

--- install.py
+++ install.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 import git
 import os
+from shutil import copyfile
+
 # git.Git().clone("https://github.com/Rykka/clickable.vim")
 
-os.system("git clone https://github.com/Rykka/clickable.vim")
+copyfile('bundle/fcitx.vim/so/fcitx.vim', './plugin/fcitx.vim')
+
 

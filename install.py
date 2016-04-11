b7fd07aae5e32189156858ae164c79f2521699ec
xushvai <xushuai@xus.io>
2016-04-11 13:01:09 +0800

    some change for install script.
    Signed-off-by: xushvai <xushuai@xus.io>

--- install.py
+++ install.py
@@ -3,15 +3,31 @@
 import git
 import os
 import subprocess
+import platform
 from shutil import copyfile
 
-# git.Git().clone("https://github.com/Rykka/clickable.vim")
+osinfo = {'type':'unknow', 'release':'unknow'}
+platform = platform.platform()
+if -1 != platform.find('Darwin'):
+    osinfo['type'] = 'mac'
+elif -1 != platform.find('Linux'):
+    osinfo['type'] = 'linux'
+elif -1 != platform.find('Windows'):
+    osinfo['type'] = 'windows'
 
-subprocess.call(["git", "submodule", "update", "--init", "--recursive"])
+subprocess.call(["git", "submodule", 
+    "update", "--init", "--recursive"])
 
-print "hello"
+# plugin: fcitx
+# --------------------------------------------------------------
+if 'mac' == osinfo['type']:
+    subprocess.call(["brew", "install", 
+        "fcitx-remote-for-osx", 
+        "--with-input-method=baidu-pinyin"])
+    copyfile('bundle/fcitx.vim/so/fcitx.vim', 
+            './plugin/fcitx.vim')
+elif 'linux' == osinfo['type']: 
+    print 'linux' 
 
-
-copyfile('bundle/fcitx.vim/so/fcitx.vim', './plugin/fcitx.vim')
 
 

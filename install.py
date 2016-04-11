92d90e6d607b0585605014fbbfded504218e73cc
xushvai <xushuai@xus.io>
2016-04-11 13:57:15 +0800

    some change.
    Signed-off-by: xushvai <xushuai@xus.io>

--- install.py
+++ install.py
@@ -1,35 +1,44 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import git
 import os
-import subprocess
 import platform
+import subprocess
 from shutil import copyfile
 
-osinfo = {'type':'unknow', 'release':'unknow'}
+class OSInfo:
+    def __init__(self, type, name):
+        self.type = type 
+        self.name = name 
+
+    type = 'unknow'
+    name = 'unknow'
+
+
+osinfo = OSInfo('unknow', 'unknow')
+
 platform = platform.platform()
 if -1 != platform.find('Darwin'):
-    osinfo['type'] = 'mac'
+    osinfo.type = 'mac'
 elif -1 != platform.find('Linux'):
-    osinfo['type'] = 'linux'
-    if -1 != platform.find('kali'): 
-        osinfo['release'] = 'kali'
+    osinfo.type = 'linux'
+    if -1 != platform.find('Kali'): 
+        osinfo.name = 'kali'
 elif -1 != platform.find('Windows'):
-    osinfo['type'] = 'windows'
+    osinfo.name = 'windows'
 
 subprocess.call(["git", "submodule", 
     "update", "--init", "--recursive"])
 
 # plugin: fcitx
 # --------------------------------------------------------------
-if 'mac' == osinfo['type']:
+if 'mac' == osinfo.type:
     subprocess.call(["brew", "install", 
         "fcitx-remote-for-osx", 
         "--with-input-method=baidu-pinyin"])
     copyfile('bundle/fcitx.vim/so/fcitx.vim', 
             './plugin/fcitx.vim')
-elif 'linux' == osinfo['type']: 
-    if 'kali' == osinfo['release']: 
+elif 'linux' == osinfo.type: 
+    if 'kali' == osinfo.name: 
         subprocess.call(["apt-get", "install", 
             "fcitx", "fcitx-sunpinyin", 
             "fcitx-libpinyin"])

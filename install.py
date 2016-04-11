4529946d3e3485ad3ef35e0c4c23a396092ea222
xushvai <xushuai@xus.io>
2016-04-11 12:03:18 +0800

    some change.
    Signed-off-by: xushvai <xushuai@xus.io>

--- install.py
+++ install.py
@@ -2,9 +2,15 @@
 # -*- coding: utf-8 -*-
 import git
 import os
+import subprocess
 from shutil import copyfile
 
 # git.Git().clone("https://github.com/Rykka/clickable.vim")
+
+subprocess.call(["git", "submodule", "update", "--init", "--recursive"])
+
+print "hello"
+
 
 copyfile('bundle/fcitx.vim/so/fcitx.vim', './plugin/fcitx.vim')
 

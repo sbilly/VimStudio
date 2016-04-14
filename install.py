cc524b0d51458538aa2fc093ef737247944d96e7
xushvai <xushuai@xus.io>
2016-04-14 17:32:27 +0800

    some change.
    Signed-off-by: xushvai <xushuai@xus.io>

--- install.py
+++ install.py
@@ -58,8 +58,6 @@ while 1:
         submodule_ver = content.split('[' + submodule_ver + ']:', 1)[1].split('/tag/', 1)[1].split('[', 1)[0]
         submodule_ver = submodule_ver[:-1]
 
-    print submodule_name + ' ' + submodule_ver
-
     is_have_dep = True
 
     try:
@@ -99,9 +97,23 @@ for name, ver in submodules.iteritems():
     subprocess.call(['git', 'checkout', ver['self']])
     os.chdir(predir)
 
-'''
+# for plugin: vimgdb or vim-lldb  
+# ---------------------------------------------------
+if 'linux' == envinfo.os_type:
+    # use vimgdb
+    patch_path = 'vimgdb-for-vim7.4/vim74.patch'
+    f = open(patch_path, 'r')
+    content = f.read()
+    f.close()
+    content = content.replace('vim74', 'vim')
+    content = content.replace('vimgdb74', 'vimgdb-for-vim7.4')
+    f = open(patch_path, 'w')
+    f.write(content)
+    f.close()
+elif 'mac' == envinfo.os_type:
+    # use vim-lldb
+    None
 
-'''
 # for plugin: fcitx
 # ---------------------------------------------------
 if 'mac' == envinfo.os_type:
@@ -126,7 +138,8 @@ if 'mac' == envinfo.os_type:
     url += filename
     decompressdir = 'clang+llvm'
     if os.path.isfile(filename):
-        subprocess.call(['rm', '-f', filename]
+        subprocess.call(['rm', '-f', filenamea])
+
     subprocess.call(['wget', url])
     subprocess.call(['mkdir', decompressdir])
 
@@ -138,9 +151,8 @@ if 'mac' == envinfo.os_type:
     predir = os.getcwd()
     dstdir = './ycm_build' 
     os.chdir(dstdir)
-    
-    # TODO: get vim use python version.
-    python = '/usr/bin/python2.7' 
+
+    python = '/usr/bin/python' + envinfo.python_ver  
     subprocess.call(['cmake', '-G', 'Unix Makefiles', 
         '-DPATH_TO_LLVM_ROOT=../' + decompressdir, 
         '-DPYTHON_EXECUTABLE=' + python, '.', 
@@ -148,44 +160,27 @@ if 'mac' == envinfo.os_type:
     subprocess.call(['make']) 
     os.chdir(predir)
 
-# create .vim in user home  
+# create .vimstudio in user home  
 # ---------------------------------------------------
 home = os.path.expanduser('~')
-newpath = home + '/.vim'
+newpath = home + '/.vimstudio'
 if not os.path.exists(newpath):
-    os.makedirs(newpath)
+    subprocess.call(['mkdir', newpath]) 
 
 # copy ...  
 # ---------------------------------------------------
-src = '.vim/bundle/'
-dst = home + '/.vim/bundle/'
-try:
-    shutil.copytree(src, dst)
-except OSError:
-    None
-src = '.vim/vimrc'
-dst = home + '/.vim/vimrc'
-try:
-    shutil.copyfile(src, dst)
-except OSError:
-    None
-src = '.vim/autoload'
-dst = home + '/.vim/autoload'
-try:
-    shutil.copytree(src, dst)
-except OSError:
-    None
-src = '.vim/ftplugin'
-dst = home + '/.vim/ftplugin'
-try:
-    shutil.copytree(src, dst)
-except OSError:
-    None
+src = './.vimrc'
+dst = newpath
+subprocess.call(['cp', src, dst]) 
+src = '.vim'
+dst = newpath
+subprocess.call(['cp', '-r', src, dst]) 
+
 '''
 
 # compile install vim 
 # ---------------------------------------------------
-# chage default config file search path
+# chage .vimrc file default search path
 filepath = './vim/src/feature.h' 
 insert_content = '#define USR_VIMRC_FILE \"~/.vimstudio/.vimrc\"'
 insert_line = 896
@@ -200,28 +195,63 @@ if -1 == tmp.find(insert_content):
     f.write(content)
     f.close()
 
-# TODO
-# about vimgdb
+# change .vim folder default search path 
+# unix os, os_unix.h include macos
+filepath = './vim/src/os_unix.h' 
+f = open(filepath, 'r')
+content = f.readlines()
+f.close()
+cond1 = False
+cond2 = False
+cond3 = False
+target_str = '~/.vim'
+replace_str = '~/.vimstudio/.vim'
+content_str = ''
+for line in content:
+    if -1 != line.find('DFLT_RUNTIMEPATH'):
+        cond1 = True
+    if -1 != line.find(target_str):
+        cond2 = True
+    if -1 == line.find(replace_str):
+        cond3 = True
+    if cond1 and cond2 and cond3:
+        line = line.replace(target_str, replace_str)
+    cond1 = False
+    cond2 = False
+    cond3 = False
+    line_str = "".join(line)
+    content_str = content_str + line_str
+f = open(filepath, 'w')
+f.write(content_str)
+f.close()
 
 # config, make, make install
 predir = os.getcwd()
 dstdir = 'vim'
 os.chdir(dstdir)
 command = ['./configure', 
+    '--prefix=/usr/local/bin',
     '--with-vim-name=vimstudio',
     '--with-features=huge', 
     '--enable-pythoninterp',
     '--enable-rubyinterp',
     '--enable-luainterp',
     '--enable-perlinterp',
+    # '--enable-gui=yes',
     '--enable-cscope',
     '--with-python-config-dir=/usr/lib/python' 
         + envinfo.python_ver + '/config/',
     ]
+if 'mac' == envinfo.os_type:
+	# command.append('--enable-gui=athena')
+	# command.append('--disable-darwin')
 if 'linux' == envinfo.os_type:
     command.append('--enable-gui=gtk2')
+    command.append('--enable-gdb')
 subprocess.call(command)
 subprocess.call(['make', 'clean']) 
 subprocess.call(['make']) 
-subprocess.call(['make', 'install']) 
+#subprocess.call(['clear']) 
+subprocess.call(['sudo', 'make', 'install']) 
+#subprocess.call(['clear']) 
 os.chdir(predir)
